af909ae5130700cd1edceabd40c83177f9a23aa3
Abolfazl-MI <abolfazlmashhadi837@gmail.com>
2022-11-27 03:11:28 -0800

    data handler class impl

---
+++ lib/core/data_staes.dart
@@ -0,0 +1,13 @@
+
+abstract class DataState<T>{
+  final T?data;
+  final String ? error;
+  DataState({this.data,this.error});
+}
+
+class DataSuccesState<T> extends DataState<T>{
+  DataSuccesState(T?data):super(data: data,error: null);
+}
+class DataFailState<T> extends DataState<T>{
+  DataFailState(String error):super(error: error,data: null);
+}
