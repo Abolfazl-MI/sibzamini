4d339331628516cf6e3a7fcd52ea5236efb7d8ef
Abolfazl-MI <abolfazlmashhadi837@gmail.com>
2022-12-01 00:07:26 -0800

    user create account logic impl

--- lib/controller/regestration/regestration_controller.dart
+++ lib/controller/regestration/regestration_controller.dart
@@ -1,13 +1,22 @@
 import 'dart:async';
 
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-
+import 'package:get/get.dart' hide FormData;
 import 'package:persian_number_utility/persian_number_utility.dart';
+import 'package:sibzamini/core/data_staes.dart';
+
 import 'package:sibzamini/core/error_code.dart';
+import 'package:sibzamini/models/services/local/location_service.dart';
+import 'package:sibzamini/models/services/local/shared_service.dart';
+import 'package:sibzamini/models/services/remote/api_services.dart';
+import 'package:sibzamini/models/user_model/user_modle.dart';
 import 'package:sibzamini/views/views.dart';
 
 class RegistrationController extends GetxController {
+  final SharedStorageService _sharedStorageService = SharedStorageService();
+  final ApiServices _apiServices = ApiServices();
+  final LocationServices _locationServices = LocationServices();
   int timeLaps = 60;
   bool isEnable = false;
   Rx<String> errorMessage = ''.obs;
@@ -24,20 +33,21 @@ class RegistrationController extends GetxController {
     return null;
   }
 
-  String ? validateName(String? value){
-    if(value==null || value.isEmpty){
-        errorMessage.value=NAME_REQUIRED;
-        return NAME_REQUIRED;
-
-    }else if(value.length<2){
-      errorMessage.value=NAME_INVALID;
+  // validats userName
+  String? validateName(String? value) {
+    if (value == null || value.isEmpty) {
+      errorMessage.value = NAME_REQUIRED;
+      return NAME_REQUIRED;
+    } else if (value.length < 2) {
+      errorMessage.value = NAME_INVALID;
       return NAME_INVALID;
-      
     }
+    return null;
   }
 
+  // validatest the pin code
   validPenCode(String? value) {
-    if (value!.length >=4) {
+    if (value!.length >= 4) {
       isEnable = true;
       update();
     } else {
@@ -46,6 +56,7 @@ class RegistrationController extends GetxController {
     }
   }
 
+  //sends otp code for first time
   sendVerfiyCode() async {
     isLoading = true;
     update();
@@ -55,6 +66,7 @@ class RegistrationController extends GetxController {
     Get.offAndToNamed(rVerifyCodeScreen);
   }
 
+  // resends the otp code
   resendVerfiyCode() async {
     Timer.periodic(Duration(seconds: 1), (timer) {
       timeLaps = timeLaps - 1;
@@ -73,5 +85,47 @@ class RegistrationController extends GetxController {
       print('sended code');
     }
     update();
+  }
+
+  // registerUser
+
+  createUserAccount({
+    required String name,
+    required String phoneNumber,
+  }) async {
+    isLoading = true;
+    update();
+    DataState<String> city = await _locationServices.getUserCityLocation();
+    if (city is DataFailState) {
+      _apiServices.cancleRequest();
+      isLoading = false;
+      update();
+      if (city.error == LOCATION_ACCESS_DENIDD) {
+        Get.snackbar('\u{1F610}' 'دسترسی مکان محدود شد', LOCATION_ACCESS_DENIDD,
+            backgroundColor: Colors.red);
+      }
+      if (city.error == DISSABLED_LOCATION_SERVICE) {
+        Get.snackbar(
+            '\u{1F610}' 'سرویس مکان یابی روشن نیست', DISSABLED_LOCATION_SERVICE,
+            backgroundColor: Colors.red);
+      }
+    }
+    FormData data = FormData.fromMap(
+        {'name': name, 'mobile': phoneNumber, 'city': city.data});
+    DataState<User> result = await _apiServices.createUserAccount(data: data);
+    if (result is DataSuccesState) {
+      isLoading = false;
+      update();
+      Get.snackbar('\u{1F642}' 'موفقیت امیز بود', 'شما وارد شدید،خوش آمدید',
+          backgroundColor: Colors.green);
+      await _sharedStorageService.saveUserToken(result.data!.token!);
+      Get.offNamed(rHomeScreen, arguments: {'user': result.data});
+    }
+    if (result is DataFailState) {
+      isLoading = false;
+      update();
+      Get.snackbar('\u{1F610}' 'مشکلی پیش اومده', SOMETHING_WENT_WRONG,
+          backgroundColor: Colors.red);
+    }
   }
 }
