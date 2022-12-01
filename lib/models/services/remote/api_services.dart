2a775b784ec6f68d4d70b8ad3f0c101e9c361e2d
Abolfazl-MI <abolfazlmashhadi837@gmail.com>
2022-12-01 00:05:48 -0800

    user create account function impl

--- lib/models/services/remote/api_services.dart
+++ lib/models/services/remote/api_services.dart
@@ -1,20 +1,36 @@
 import "package:dio/dio.dart";
+import 'package:sibzamini/core/data_staes.dart';
+import 'package:sibzamini/core/error_code.dart';
+import 'package:sibzamini/models/services/remote/api_const.dart';
+import 'package:sibzamini/models/user_model/user_modle.dart';
 
 class ApiServices {
- final Dio _dio = Dio(BaseOptions(baseUrl: 'http://sunict.ir/api/v1', headers: {
-    'Accept': 'application/json',
-    'Authorization': 'x5rjvhs4dnq3k4ael7yfrr0xk4et9gzumbgyzw88q3u6yp529q',
-  },),);
-// 442->phonenumber
+  final Dio _dio = Dio(
+    BaseOptions(
+      baseUrl: 'http://sunict.ir/api/v1',
+      headers: {
+        'Accept': 'application/json',
+        'Authorization': 'x5rjvhs4dnq3k4ael7yfrr0xk4et9gzumbgyzw88q3u6yp529q',
+      },
+    ),
+  );
 
 
-  createUserAccount({
-    required String phoneNumber,
-    required String name,
-    required String city,
+  Future <DataState<User>> createUserAccount({
+    required FormData data
   }) async {
-
+    try {
+      Response response= await _dio.post(register, data:data);
+      if(response.statusCode==200){
+        User user=User.fromJson(response.data);
+        return DataSuccesState(user);
+      }
+      return DataFailState(SOMETHING_WENT_WRONG);
+    } catch (e) {
+      return DataFailState(SOMETHING_WENT_WRONG);
+    }
   }
+
   confirmUserOtp({required String otpCode}) async {
     // TODO: Impelement the user otp code confirmation
   }
@@ -41,5 +57,10 @@ class ApiServices {
 
   categoriesList() {
     // TODO:implement the  get categories
+  }
+
+
+  cancleRequest(){
+     return CancelToken().cancel();
   }
 }
