65b7039164599ff6f9aae8811de08c95488251e8
Abolfazl-MI <abolfazlmashhadi837@gmail.com>
2022-12-04 03:32:12 -0800

    error constans updated

--- lib/core/error_code.dart
+++ lib/core/error_code.dart
@@ -10,3 +10,4 @@ const String NAME_REQUIRED='وارد کردن نام الزامی است';
 const String NAME_INVALID='نام را درست وارد کنید';
 const String NO_INTERNET_CONNECTION="به اینترنت متصل نیستید، اتصال خود را چک کنید";
 const String VPN_CONNCETION_DETECTED='لطفا اتصال فیلترشکن را غیر فعال کنید';
+const String SALON_NOT_FOUND="سالن با مشخصات ذکر شده، بافت نشد";
