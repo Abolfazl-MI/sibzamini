1d897162b2006d0b943455b6babf582984183e71
Abolfazl-MI <abolfazlmashhadi837@gmail.com>
2022-12-06 06:20:53 -0800

    added homefeed function and salon category function

--- lib/controller/home/home_controller.dart
+++ lib/controller/home/home_controller.dart
@@ -1,11 +1,14 @@
+import 'dart:developer';
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
-
+import 'package:sibzamini/models/category_model/category_model.dart';
 import 'package:sibzamini/core/data_staes.dart';
 import 'package:sibzamini/models/category_model/category_model.dart';
 import 'package:sibzamini/models/salon_model/salon_model.dart';
+import '';
 import 'package:sibzamini/services/local/shared_service.dart';
 import 'package:sibzamini/services/remote/api_const.dart';
 import 'package:sibzamini/services/remote/api_services.dart';
@@ -20,9 +23,10 @@ class HomeController extends GetxController {
   List<Salon> bestSalonsList = [];
   List<Salon> newestSalonList = [];
   List<Salon> salonsBasedOnCategory = [];
-
+  List<SalonCategory> salonCategories = [];
   GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
   bool isLoading = false;
+  bool isCategoryLoadign=false;
   //methods
   // get best Salons
   Future<void> getBestSalons({required String cityName}) async {
@@ -63,14 +67,14 @@ class HomeController extends GetxController {
 
   // search Salons
   Future<void> searchSalons({required String salonQuery}) async {}
-  // get categories
+  // get salon base on categories
   Future<void> getSalonByCategories({required SalonCategory category}) async {
     String? userCity = await _storageService.getUserCity();
     if (userCity == null) {
       // todo : should get user current city location
     }
     DataState<List<Salon>> result = await _apiServices.getSalonByCategories(
-        city: userCity ?? "Tehran", category: category);
+        city: userCity!, category: category);
     if (result is DataSuccesState) {
       salonsBasedOnCategory = result.data!;
       update();
@@ -80,8 +84,43 @@ class HomeController extends GetxController {
     }
   }
 
+  // get salons category
+  Future<void>getSalonCategories()async{
+    isCategoryLoadign=true;
+    update();
+    DataState<List<SalonCategory>> resualt= await _apiServices.categoriesList();
+    if(resualt is DataSuccesState){
+      salonCategories=resualt.data!;
+      isCategoryLoadign=false;
+      update();
+    }
+    if(resualt is DataFailState){
+      isCategoryLoadign=false;
+      update();
+       Get.snackbar('\u{1F610}' 'مشکلی پیش آمده', resualt.error!,
+          backgroundColor: Colors.red);
+    }
+  }
+
+
+  Future<void> getHomeFeedSalons({int? limit}) async {
+    // todo should get user current city
+    getNewesSalons(cityName: 'tehran');
+    getBestSalons(cityName: 'tehran');
+    getSalonCategories();
+    print(bestSalonsList);
+    print(newestSalonList);
+  }
+
   // opens the home darwer
   void openDrawer() {
     scaffoldKey.currentState!.openDrawer();
+  }
+
+  @override
+  void onInit() {
+    // TODO: implement onInit
+    super.onInit();
+    getHomeFeedSalons();
   }
 }
