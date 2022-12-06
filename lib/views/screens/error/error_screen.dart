9488d8dcceeaa48c98ac09d5ac864ff8471c723b
Abolfazl-MI <abolfazlmashhadi837@gmail.com>
2022-12-06 06:05:28 -0800

    error animation and screen ui updated

--- lib/views/screens/error/error_screen.dart
+++ lib/views/screens/error/error_screen.dart
@@ -1,29 +1,57 @@
-
-
 import 'package:flutter/material.dart';
+
+import 'package:get/get_core/get_core.dart';
+import 'package:get/get_navigation/get_navigation.dart';
 import 'package:lottie/lottie.dart';
+
 import 'package:sibzamini/core/error_code.dart';
 import 'package:sibzamini/gen/assets.gen.dart';
+import 'package:sibzamini/views/global/colors/colors.dart';
 import 'package:sibzamini/views/global/constants/app_text_themes.dart';
+import 'package:sibzamini/views/routes/app_route_names.dart';
 
-class ErrorScreen extends StatelessWidget{
+class ErrorScreen extends StatelessWidget {
+  const ErrorScreen({super.key});
+
   @override
-  Widget build(BuildContext context){
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Transform.scale(
-            scale: 0.9,
-            child: Lottie.asset(Assets.lotties.error)), 
-          Text(SOMETHING_WENT_WRONG,
-          
-            style: AppTextTheme.caption.copyWith(
-              fontWeight: FontWeight.bold
-            )), 
-            Container(height: 40,)
-        ],
+  Widget build(BuildContext context) {
+    // String error_code=Get.argument['error'];
+    String error_code = Get.arguments['error'];
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Transform.scale(
+                scale: 0.87, child: Lottie.asset(Assets.lotties.error)),
+            Text(
+              error_code,
+              style: AppTextTheme.caption.copyWith(fontWeight: FontWeight.bold),
+            ),
+            if (error_code == NO_INTERNET_CONNECTION)
+              InkWell(
+                onTap: (){
+                  Get.offAllNamed(rSplashScreen);
+                },
+                child: Container(
+                  width: 150,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12),
+                    color: SolidColors.primaryBlue,
+                  ),
+                  child: Center(
+                    child: Text(
+                      'دوباره تلاش کنید',
+                      style: AppTextTheme.caption.copyWith(color: Colors.white),
+                    ),
+                  ),
+                ),
+              )
+          ],
+        ),
       ),
     );
   }
