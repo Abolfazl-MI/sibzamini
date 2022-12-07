414c302f1f6e8ba621daa17d8cb9589e03f53ddc
Abolfazl-MI <abolfazlmashhadi837@gmail.com>
2022-12-06 22:30:15 -0800

    added categories path

--- lib/services/remote/api_const.dart
+++ lib/services/remote/api_const.dart
@@ -1,4 +1,4 @@
-// AUTH
+ // AUTH
 const String register = '/register';
 const String login = '/login';
 const String loginCheck = '/logincheck';
@@ -9,3 +9,4 @@ const String salonServices = '/services'; //! id should added
 const String salonInfo = '/salon'; //! id shold added
 const String searchSalon='/search';
 const String salonComments='/comments/list';
+const String categories='/cats';
