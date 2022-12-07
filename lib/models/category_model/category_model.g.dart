6f7119e0e2a7b2f7c1cb42d03e6ec49d02223371
abolfazl <abolfazlmashhadi837@gmail.com>
2022-12-07 04:07:23 -0800

    regenerated category model

--- lib/models/category_model/category_model.g.dart
+++ lib/models/category_model/category_model.g.dart
@@ -6,14 +6,14 @@ part of 'category_model.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-ServiceCategory _$SalonCategoryFromJson(Map<String, dynamic> json) =>
+ServiceCategory _$ServiceCategoryFromJson(Map<String, dynamic> json) =>
     ServiceCategory(
       id: json['id'] as int?,
       name: json['name'] as String?,
       slug: json['slug'] as String?,
     );
 
-Map<String, dynamic> _$SalonCategoryToJson(ServiceCategory instance) =>
+Map<String, dynamic> _$ServiceCategoryToJson(ServiceCategory instance) =>
     <String, dynamic>{
       'id': instance.id,
       'name': instance.name,
