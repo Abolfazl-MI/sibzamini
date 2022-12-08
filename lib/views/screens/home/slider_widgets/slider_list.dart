cfc2b6705d59493c3e53c0345b315395dc997087
abolfazl <abolfazlmashhadi837@gmail.com>
2022-12-08 09:38:07 -0800

    updated dua to get data from constructor

--- lib/views/screens/home/slider_widgets/slider_list.dart
+++ lib/views/screens/home/slider_widgets/slider_list.dart
@@ -1,30 +1,32 @@
 import 'package:flutter/material.dart';
+
 import 'package:get/get.dart';
+
 import 'package:sibzamini/gen/assets.gen.dart';
+import 'package:sibzamini/models/salon_model/salon_model.dart';
 import 'package:sibzamini/views/screens/home/slider_widgets/slider_widget.dart';
 import 'package:sibzamini/views/views.dart';
 
 class SliderList extends StatelessWidget {
-  // TODO GET THE LIST AND USE IT HERE
-
-  const SliderList({Key? key}) : super(key: key);
-// TODO: SHOULD GET LIST OF AVALABLE STH THEN USE
+  final List<Salon> salons;
+  const SliderList({Key? key, required this.salons}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
     return Container(
       width: width,
-      height: 220,
+      height: 240,
       // color: Colors.amber,
       child: ListView.builder(
-        itemCount: 20,
+        physics: BouncingScrollPhysics(),
+        itemCount:salons.length,
         scrollDirection: Axis.horizontal,
         itemBuilder: (context, index) {
-          // TODO: Attention HARD CODE HERE SHOULD REPLACE WITH REALL DATA
+          
           return SliderWidget(
-            title: 'سالن‌زیبایی‌ایمان',
-            ratingCount: 2,
-            imageUrl: Assets.images.modernBeautySalonInterior2.path,
+            title: salons[index].name!,
+            ratingCount:salons[index].rate!.toDouble(),
+            imageUrl: 'https://sunict.ir${salons[index].pic}',
             onTap: () {
               Get.toNamed(rDetailScreen);
             },
--- lib/views/screens/home/slider_widgets/slider_widget.dart
+++ lib/views/screens/home/slider_widgets/slider_widget.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:lottie/lottie.dart';
 import 'package:rate_in_stars/rate_in_stars.dart';
+
 import 'package:sibzamini/gen/assets.gen.dart';
 import 'package:sibzamini/views/global/colors/solid_colors.dart';
 
@@ -26,29 +29,51 @@ class SliderWidget extends StatelessWidget {
       padding: EdgeInsets.all(5),
       child: SizedBox(
         width: 180,
-        height: 159,
+        height: 170,
         child: InkWell(
           onTap: onTap,
           child: Card(
             elevation: 0,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12),
+            ),
             child: Column(
               // mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                // image section
-                // TODO: USE CACHENETWORK IMAGE
-                Container(
-                  width: 180,
-                  height: 130,
-                  decoration: BoxDecoration(
-                    // color: Colors.green,
-                    image: DecorationImage(
-                        image: AssetImage(
-                         imageUrl
-                        ),
-                        fit: BoxFit.cover),
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(12),
-                      topRight: Radius.circular(12),
+                CachedNetworkImage(
+                  imageUrl: imageUrl,
+                  imageBuilder: (context, imageProvider) => Container(
+                    width: 180,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      // color: Colors.green,
+                      image: DecorationImage(
+                          image: imageProvider, fit: BoxFit.cover),
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(12),
+                        topRight: Radius.circular(12),
+                      ),
+                    ),
+                  ),
+                  errorWidget: (context, url, error) => Container(
+                    width: 180,
+                    height: 50,
+                    color: Colors.grey.withOpacity(0.6),
+                    child: Center(
+                      child: Icon(
+                        Icons.image_not_supported_outlined,
+                        color: Colors.red,
+                      ),
+                    ),
+                  ),
+                  placeholder: (context, url) => Container(
+                    width: 180,
+                    height: 50,
+                    child: Center(
+                      child: Transform.scale(
+                        scale: 0.4,
+                        child: Lottie.asset(Assets.lotties.loading),
+                      ),
                     ),
                   ),
                 ),
@@ -81,9 +106,6 @@ class SliderWidget extends StatelessWidget {
                   ],
                 )
               ],
-            ),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(12),
             ),
           ),
         ),
