32e4188d55d2d65097be7a7c60868a866f26376f
abolfazl <abolfazlmashhadi837@gmail.com>
2022-12-08 23:53:05 -0800

    added functionality to fetch salon detail

--- lib/controller/detail/detail_controller.dart
+++ lib/controller/detail/detail_controller.dart
@@ -1,7 +1,10 @@
+import 'dart:developer';
+
 import 'package:get/get.dart';
 import 'package:sibzamini/core/data_staes.dart';
 import 'package:sibzamini/models/comment_model/comment_model.dart';
 import 'package:sibzamini/models/salon_model/salon_model.dart';
+import 'package:sibzamini/models/services_model/services_model.dart';
 import 'package:sibzamini/services/remote/api_services.dart';
 import 'package:sibzamini/views/routes/app_route_names.dart';
 
@@ -16,6 +19,7 @@ class DetailController extends GetxController {
   bool isLoading = false;
   Salon? salondetail;
   List<Comment>? salonComments;
+  List<SalonService>? salonServices;
   // methods
   // bottom navigation
   void updateSelectedIndex(int newValue) {
@@ -29,7 +33,7 @@ class DetailController extends GetxController {
     update();
   }
 
-  Future<void> salonDeatail({required int id}) async {
+  Future<void> _getSalonDeatail({required int id}) async {
     DataState<Salon> salonDetailResult =
         await _apiServices.getSalonDetail(id: id);
     if (salonDetailResult is DataSuccesState) {
@@ -42,7 +46,7 @@ class DetailController extends GetxController {
     }
   }
 
-  Future<void> getSalonComments({required int id}) async {
+  Future<void> _getSalonComments({required int id}) async {
     DataState<List<Comment>> results =
         await _apiServices.getSalonComments(id: id);
     if (results is DataSuccesState) {
@@ -52,6 +56,29 @@ class DetailController extends GetxController {
     if (results is DataFailState) {
       // handle the error
     }
+  }
+  Future<void> _getSalonServices({required int id})async{
+    DataState<List<SalonService>> result= await _apiServices.getSingleSalonServices(salonId: id);
+    if(result is DataSuccesState){
+      salonServices=result.data;
+      update();
+    }
+    if(result is DataFailState){
+      salonServices=[];
+      update();
+    }
+
+  }
+
+  Future<void>getSalonDetail({required int id})async{
+    log('============fetching slon details============');
+    isLoading=true;
+    update();
+    await _getSalonDeatail(id: id);
+    await _getSalonServices(id: id);
+    await _getSalonComments(id: id);
+    isLoading=false;
+    update();
   }
 
   getOpinionBasedRatingCount() {
@@ -66,5 +93,12 @@ class DetailController extends GetxController {
     } else if (rateToSalon == 5) {
       return 'عالی';
     }
+  }
+
+  @override
+  void onInit() {
+    // TODO: implement onInit
+    super.onInit();
+    getSalonDetail(id: Get.arguments['id']);
   }
 }
